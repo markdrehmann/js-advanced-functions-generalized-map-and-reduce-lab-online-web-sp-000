ac9d9b2e0e5af85d2090b573498f89676a8522e7
markdrehmann <markdrehmann@gmail.com>
2021-03-23 10:56:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(source, callback) {
+  let result = []
+  for (let i = 0; i < source.length; i++) {
+    let el = source[i]
+    result.push(callback(el))
+  }
+  return result
+}
+
+function reduce(source, callback, start) {
+  let result = !!start ? start : source[0]
+  let i = !!start ? 0 : 1
+
+  for (; i < source.length; i++) {
+    result = callback(source[i], result)
+  }
+  return result
+}
